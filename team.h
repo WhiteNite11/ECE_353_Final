395da985743cf1c8526a0d345bbc842edeed0f34
David <smartydude010@hotmail.com>
2013-12-02 22:49:44 -0600

    Updated Team Header

--- team.h
+++ team.h
@@ -1,7 +1,7 @@
 #ifndef __TEAM_H__
 #define __TEAM_H__
 
-char teamNumber[]   = "00";
-char teamMembers[]  = "Student1,Student2";
+char teamNumber[]   = "10";
+char teamMembers[]  = "David Gardner, Justin Russo";
 
 #endif 
